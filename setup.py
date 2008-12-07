a6a06e80f038c355a436043d8934ad4542641678
garbas <rok@garbas.si>
2008-12-07 00:11:32 +0000

    collective.jqueryui initial import
    git-svn-id: https://svn.plone.org/svn/collective/collective.jqueryui/trunk@77006 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join('collective', 'jqueryui', 'version.txt')).read()
+
+setup(name='collective.jqueryui',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone jquery jqueryui',
+      author='Rok Garbas',
+      author_email='rok.garbas@gmail.com',
+      url='http://',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
