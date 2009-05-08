7ab9944caf57d30760f1d3ae7493864f32809517
hannosch <hanno@hannosch.eu>
2009-05-08 20:52:27 +0000

    Make proper use of the Acquistion API and use the aq_parent function.
    git-svn-id: https://svn.plone.org/svn/collective/collective.jqueryui/trunk@85737 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/jqueryui/patch.py
+++ collective/jqueryui/patch.py
@@ -1,3 +1,4 @@
+from Acquisition import aq_parent
 import Products.Five.browser.resource
 
 def do():
@@ -5,6 +6,6 @@ def do():
     def DirectoryResource_get(*args, **kwargs):
         resource = _old_get(*args, **kwargs)
         if getattr(resource, '__roles__', None) is None:
-            resource.__roles__ = resource.aq_parent.__roles__
+            resource.__roles__ = aq_parent(resource).__roles__
         return resource
     Products.Five.browser.resource.DirectoryResource.get = DirectoryResource_get
